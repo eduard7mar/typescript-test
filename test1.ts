8a6db16ac1d62f344c2dd30c99720e4e6583a928
eduard7mar <edwardmargaryan@gmail.com>
2024-02-05 20:51:08 +0200

    added test1

--- test1.ts
+++ test1.ts
@@ -1,38 +1,50 @@
 const electricityUserData = {
-	readings: 95,
-	units: "kWt",
-	mode: "double",
+  readings: 95,
+  units: "kWt",
+  mode: "double",
 };
 
 const waterUserData = {
-	readings: 3,
-	units: "m3",
+  readings: 3,
+  units: "m3",
 };
 
-const elRate = 0.45;
-const wRate = 2;
+const elRate: number = 0.45;
+const wRate: number = 2;
 
-const monthPayments = [0, 0]; // [electricity, water]
+const monthPayments: number[] = [0, 0]; // [electricity, water]
 
-const calculatePayments = (elData, wData, elRate, wRate) => {
-	if (elData.mode === "double" && elData.readings < 50) {
-		monthPayments[0] = elData.readings * elRate * 0.7;
-	} else {
-		monthPayments[0] = elData.readings * elRate;
-	}
+const calculatePayments = (
+  { readings, mode }: { readings: number; mode: string },
+  wData: { readings: number },
+  elRate: number,
+  wRate: number
+): void => {
+  if (mode === "double" && readings < 50) {
+    monthPayments[0] = readings * elRate * 0.7;
+  } else {
+    monthPayments[0] = readings * elRate;
+  }
 
-	monthPayments[1] = wData.readings * wRate;
+  monthPayments[1] = wData.readings * wRate;
 };
 
 calculatePayments(electricityUserData, waterUserData, elRate, wRate);
 
-const sendInvoice = (monthPayments, electricityUserData, waterUserData) => {
-	const text = `    Hello!
+const sendInvoice = (
+  [el, water]: number[],
+  electricityUserData: { readings: number; units: string },
+  waterUserData: { readings: number; units: string }
+): string => {
+  const text = `    Hello!
     This month you used ${electricityUserData.readings} ${electricityUserData.units} of electricity
-    It will cost: ${monthPayments[0]}€
+    It will cost: ${el}€
     
     This month you used ${waterUserData.readings} ${waterUserData.units} of water
-    It will cost: ${monthPayments[1]}€`;
+    It will cost: ${water}€`;
 
-	return text;
+  return text;
 };
+
+const invoice = sendInvoice(monthPayments, electricityUserData, waterUserData);
+console.log(invoice);
